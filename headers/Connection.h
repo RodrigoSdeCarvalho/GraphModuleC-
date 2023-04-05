26edbb364ebbf6b2c5935c51e70760fe77c11214
carvalho <carvalho@lisha.ufsc.br>
2023-04-05 17:30:59 -0300

    Connection header

---
+++ headers/Connection.h
@@ -0,0 +1,37 @@
+#ifndef Connection_h
+#define Connection_h
+
+#include <iostream>
+#include <vector>
+#include "Node.h"
+
+using namespace std;
+
+namespace GraphModule
+{
+    class Connection
+    {
+        public:
+            Connection(float weight, Node* startNode, Node* endNode, bool goesBothWays);
+
+            float getWeight();
+            Node* getStartNode();
+            Node* getEndNode();
+            bool getGoesBothWays();
+
+            void setWeight(float weight);
+            void setGoesBothWays(bool goesBothWays);
+
+            tuple<Node*, Node*> getNodes();
+
+            ~Connection();
+
+        private:
+            float weight;
+            Node* startNode;
+            Node* endNode;
+            bool goesBothWays;
+    };
+}
+
+#endif
