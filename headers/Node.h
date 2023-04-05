b4d715236089ac98d956e4adad9637b3b1c6feab
carvalho <carvalho@lisha.ufsc.br>
2023-04-05 17:31:09 -0300

    Node header

---
+++ headers/Node.h
@@ -0,0 +1,59 @@
+#ifndef Node_h
+#define Node_h
+
+#include <iostream>
+#include <vector> // Will perhaps be replaced by list if adds and removes are more common than reads. Vector is cache friendly.
+#include "Connection.h"
+
+using namespace std;
+
+namespace GraphModule
+{
+    class Node
+    {
+        public:
+            Node(int number, string name = "");
+            
+            int getNumber();
+            
+            string getName();
+
+            vector<Connection*> getConnections();
+
+            void addConnections(vector<Connection*> connectionsToAdd);
+
+            Connection* getConnectionWith(Node* NodeConnectedOnTheOtherEnd);
+
+            Connection* getOutgoingConnectionTo(Node* NodeConnectedOnTheOtherEnd);
+
+            vector<Node*> getNeighbours();
+
+            vector<Connection*> getIncomingConnections();
+
+            vector<Connection*> getOutgoingConnections();
+
+            void addConnection(Connection* connectionToAdd);
+
+            void addIncomingConnection(Connection* connectionToAdd);
+
+            void addOutgoingConnection(Connection* connectionToAdd);
+
+            vector<Node*> getOutgoingNeighbours();
+
+            vector<tuple<Node*, Connection*>> getOutgoingNeighboursWithConnection();
+
+            vector<Node*> getIncomingNeighbours();
+
+            ~Node();
+
+        private:
+            int number;
+            string name;
+            vector<Connection*> connections;
+            vector<Connection*> incomingConnections;
+            vector<Connection*> outgoingConnections;
+
+    };
+}
+
+#endif 
