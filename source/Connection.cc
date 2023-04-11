5e4e6058d590bdd0a1c6adb0c35d3e7a3048c593
carvalho <carvalho@lisha.ufsc.br>
2023-04-11 10:39:05 -0300

    Implemented Connection.cc

--- source/Connection.cc
+++ source/Connection.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <tuple>
 #include "Connection.h"
+#include <tuple>
+#include "Node.h"
 
 using namespace std;
 using namespace GraphModule;
@@ -12,4 +14,46 @@ Connection::Connection(float weight, Node* startNode, Node* endNode, bool goesBo
     this->startNode = startNode;
     this->endNode = endNode;
     this->goesBothWays = goesBothWays;
+}
+
+float Connection::getWeight()
+{
+    return this->weight;
+}
+
+Node* Connection::getStartNode()
+{
+    return this->startNode;
+}
+
+Node* Connection::getEndNode()
+{
+    return this->endNode;
+}
+
+bool Connection::getGoesBothWays()
+{
+    return this->goesBothWays;
+}
+
+void Connection::setWeight(float weight)
+{
+    this->weight = weight;
+}
+
+void Connection::setGoesBothWays(bool goesBothWays)
+{
+    this->goesBothWays = goesBothWays;
+}
+
+//Be careful for bugs. This function is not tested.
+tuple<Node*, Node*> Connection::getNodes()
+{
+    return make_tuple(this->startNode, this->endNode);
+}
+
+Connection::~Connection()
+{
+    // Deletes all attributes.
+    // A node deletes all its connections, but connections don't delete their nodes.
 }
