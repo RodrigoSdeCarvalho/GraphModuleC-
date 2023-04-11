c4ed7ec0da692f40d1b252cd258f87a0f93192c3
carvalho <carvalho@lisha.ufsc.br>
2023-04-11 09:34:39 -0300

    Finished Node.cc. Look out for tuple bugs

--- source/Node.cc
+++ source/Node.cc
@@ -1,7 +1,10 @@
 #include <iostream>
-#include <vector> // Will perhaps be replaced by list if adds and removes are more common than reads. Vector is cache friendly.
+#include <vector>
+// Adds are only done when the graph is created and file is read. Reads are done a lot of times during program execution.
+// Vector is cache friendly. Hence, reading is better through vector.
 #include <tuple>
 #include "Node.h"
+#include "Connection.h"
 
 using namespace std;
 using namespace GraphModule;
@@ -25,4 +28,173 @@ string Node::getName()
 vector<Connection*> Node::getConnections()
 {
     return connections;
+}
+
+void Node::addConnections(vector<Connection*> connectionsToAdd)
+{
+    for (int connIndex = 0; connIndex < connectionsToAdd.size(); connIndex++)
+    {
+        this->addConnection(connectionsToAdd[connIndex]);
+    }
+}
+
+void Node::addConnection(Connection* connectionToAdd)
+{
+    this->connections.push_back(connectionToAdd);
+}
+
+Connection* Node::getConnectionWith(Node* NodeConnectedOnTheOtherEnd)
+{
+    for (int connIndex = 0; connIndex < this->connections.size(); connIndex++)
+    {
+        Connection* conn = this->connections[connIndex];
+
+        Node* startNode = conn->getStartNode();
+        Node* endNode = conn->getEndNode();
+
+        if ((startNode == NodeConnectedOnTheOtherEnd) or (endNode == NodeConnectedOnTheOtherEnd))
+        {
+            return conn;
+        }
+    }
+}
+
+Connection* Node::getOutgoingConnectionTo(Node* NodeConnectedOnTheOtherEnd)
+{
+    for (int connIndex = 0; connIndex < this->outgoingConnections.size(); connIndex++)
+    {
+        Connection* conn = this->outgoingConnections[connIndex];
+
+        Node* endNode = conn->getEndNode();
+
+        if (endNode == NodeConnectedOnTheOtherEnd)
+        {
+            return conn;
+        }
+    }
+}
+
+vector<Node*> Node::getNeighbours()
+{
+    vector<Node*> neighbours;
+
+    for (int connIndex = 0; connIndex < this->connections.size(); connIndex++)
+    {
+        Connection* conn = this->connections[connIndex];
+
+        Node* startNode = conn->getStartNode();
+        Node* endNode = conn->getEndNode();
+
+        if (startNode == this)
+        {
+            neighbours.push_back(endNode);
+        }
+        else if (endNode == this)
+        {
+            neighbours.push_back(startNode);
+        }
+    }
+
+    return neighbours;
+}
+
+vector<Connection*> Node::getIncomingConnections()
+{
+    return this->incomingConnections;
+}
+
+vector<Connection*> Node::getOutgoingConnections()
+{
+    return this->outgoingConnections;
+}
+
+void Node::addConnection(Connection* connectionToAdd)
+{
+    this->connections.push_back(connectionToAdd);
+}
+
+void Node::addIncomingConnection(Connection* connectionToAdd)
+{
+    this->incomingConnections.push_back(connectionToAdd);
+}
+
+void Node::addOutgoingConnection(Connection* connectionToAdd)
+{
+    this->outgoingConnections.push_back(connectionToAdd);
+}
+
+vector<Node*> Node::getOutgoingNeighbours()
+{
+    vector<Node*> neighbours;
+
+    for (int connIndex = 0; connIndex < this->outgoingConnections.size(); connIndex++)
+    {
+        Connection* conn = this->outgoingConnections[connIndex];
+
+        Node* endNode = conn->getEndNode();
+
+        neighbours.push_back(endNode);
+    }
+
+    return neighbours;
+}
+
+//Be careful for bugs. This function is not tested.
+vector<tuple<Node*, Connection*>> Node::getOutgoingNeighboursWithConnection()
+{
+    vector<tuple<Node*, Connection*>> neighbours;
+
+    for (int connIndex = 0; connIndex < this->outgoingConnections.size(); connIndex++)
+    {
+        Connection* conn = this->outgoingConnections[connIndex];
+
+        Node* endNode = conn->getEndNode();
+
+        neighbours.push_back(make_tuple(endNode, conn));
+    }
+
+    return neighbours;
+}
+
+vector<Node*> Node::getIncomingNeighbours()
+{
+    vector<Node*> neighbours;
+
+    for (int connIndex = 0; connIndex < this->incomingConnections.size(); connIndex++)
+    {
+        Connection* conn = this->incomingConnections[connIndex];
+
+        Node* startNode = conn->getStartNode();
+
+        neighbours.push_back(startNode);
+    }
+
+    return neighbours;
+}
+
+Node::~Node() //Look out for memory leaks. This deletes all connections since when an node is deleted, all connections shall be deleted.
+{
+    if (this->connections.size() > 0)
+    {
+        for (int connIndex = 0; connIndex < this->connections.size(); connIndex++)
+        {
+            delete this->connections[connIndex];
+        }
+    }
+
+    if (this->incomingConnections.size() > 0)
+    {
+        for (int connIndex = 0; connIndex < this->incomingConnections.size(); connIndex++)
+        {
+            delete this->incomingConnections[connIndex];
+        }
+    }
+
+    if (this->outgoingConnections.size() > 0)
+    {
+        for (int connIndex = 0; connIndex < this->outgoingConnections.size(); connIndex++)
+        {
+            delete this->outgoingConnections[connIndex];
+        }
+    }
 }
