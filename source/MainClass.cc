4e456fa3f88c318f090481f844a69022cbc2144d
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-04-19 15:22:02 -0300

    Calling Dijkstra

--- source/MainClass.cc
+++ source/MainClass.cc
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
+#include <cstdio>
+
 #include "MainClass.h"
 #include "Graph.h"
+#include "Node.h"
 
 using namespace std;
+using namespace filesystem;
 using namespace GraphModule;
 
 void MainClass::A1Main()
 {
-    //IMPLEMENT CODE TO ANSWER ALL A1 QUESTIONS.
+    path current_path = filesystem::current_path();
+    string file_name = string(current_path.c_str()) + "/inputs/fln_pequena.txt";
+    Graph graph = Graph(file_name);
+
+    // for (int i = 0; i < graph.getNumberOfVertices(); i++)
+    // {
+    //     cout << "Node: " << graph.getNodes()[i] << endl;
+    //     for (int j = 0; j < graph.getNodes()[i]->getConnections().size(); j++)
+    //     {
+    //         cout << "Connection: " << graph.getNodes()[i]->getConnections()[j] << endl;
+    //     }
+    // }
+
+    for (int i = 0; i < graph.getNumberOfVertices(); i++)
+    {  
+        cout << "CALLING DIJKSTRA FOR NODE " << i << endl;
+         graph.dijkstra(i);
+    }
 }
 
 void MainClass::A2Main()
