6ac42a0dc7500322cf917c0ba958c6eb4ef87c6e
carvalho <carvalho@lisha.ufsc.br>
2023-04-24 01:21:22 -0300

    Changed Graph to abstract

---
+++ headers/AbstractGraph.h
@@ -0,0 +1,39 @@
+#ifndef AbstractGraph_h
+#define AbstractGraph_h
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+namespace GraphModule
+{
+    class Node;
+    class Connection;
+
+    class AbstractGraph
+    {
+        public:
+            AbstractGraph();
+
+            int getNumberOfVertices();
+
+            vector<int> getNodeKeys();
+
+            vector<shared_ptr<Node>> getNodes();
+
+            virtual int getDegreeOfNode(int nodeKey) = 0;
+
+            void addNode(shared_ptr<Node> node);
+
+            ~AbstractGraph();
+
+        protected:
+            int numberOfVertices;
+            vector<shared_ptr<Node>> nodes;
+    };
+}
+
+#endif
---
+++ source/AbstractGraph.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <memory>
+
+#include "AbstractGraph.h"
+#include "Node.h"
+#include "Connection.h"
+#include "MinHeap.h"
+#include "HeapNode.h"
+
+using namespace std;
+using namespace GraphModule;
+
+AbstractGraph::AbstractGraph()
+{
+    this->numberOfVertices = 0;
+}
+
+int AbstractGraph::getNumberOfVertices()
+{
+    return this->numberOfVertices;
+}
+
+vector<int> AbstractGraph::getNodeKeys()
+{
+    vector<int> nodeKeys;
+    for (int i = 0; i < this->nodes.size(); i++)
+    {
+        nodeKeys.push_back(this->nodes[i]->getNumber());
+    }
+    return nodeKeys;
+}
+
+vector<shared_ptr<Node>> AbstractGraph::getNodes()
+{
+    return this->nodes;
+}
+
+void AbstractGraph::addNode(shared_ptr<Node> node)
+{
+    this->nodes.push_back(node);
+    this->numberOfVertices++;
+}
+
+AbstractGraph::~AbstractGraph()
+{
+    
+}
