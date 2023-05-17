421a7b48ecc4802ca22fe10ca22fd660f8642e89
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-05-17 13:57:20 -0300

    Changes

--- source/DirectedGraph.cc
+++ source/DirectedGraph.cc
@@ -231,18 +231,12 @@ vector<shared_ptr<Node>> DirectedGraph::topologicalSorting()
 
 void DirectedGraph::DFSVisitTopologicalSorting(int v, vector<bool> &C, vector<int> &F, vector<int> &T, vector<shared_ptr<Node>> &O, int &time)
 {
-    cout << "\n\nCHAMANDO COM v = " << nodes[v]->getName() << endl;
     C[v] = true;
     time++;
     T[v] = time;
 
-    for (int i = 0; i < C.size(); i++)
-    {
-        cout << "C[" << i << "] = " << C[i] << " and T [" << i << "] = " << T[i] << endl;
-    }
-
     vector<shared_ptr<Node>> outgoingNeighbours = this->nodes[v]->getOutgoingNeighbours();
-    cout << "OUTGOINGNEIGHBOURS SIZE FOR v = " << v << " = " << outgoingNeighbours.size() << endl;
+
     for (int i = 0; i < outgoingNeighbours.size(); i++)
     {
         shared_ptr<Node> neighbour = outgoingNeighbours[i];
@@ -255,19 +249,9 @@ void DirectedGraph::DFSVisitTopologicalSorting(int v, vector<bool> &C, vector<in
 
     time++;
     F[v] = time;
-    cout << "\n-------------------------------------------------------------" << endl;
-    cout << "\nBEFORE: \n" << endl;
-    for (int i = 0; i < O.size(); i++)
-    {
-        cout << O[i]->getName() << " -> ";
-    }
+
     O.insert(O.begin(), nodes[v]);
-    cout << "\n-------------------------------------------------------------" << endl;
-    cout << "\nAFTER: \n" << endl;
-    for (int i = 0; i < O.size(); i++)
-    {
-        cout << O[i]->getName() << " -> ";
-    }
+
 }
 
 void DirectedGraph::printTopologicalSorting(vector<shared_ptr<Node>> O)
